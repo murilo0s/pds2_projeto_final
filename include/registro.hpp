dbb65ca02aaa741260c76776883c15628f746e4d
Abner-Sn <samuelabner52@gmail.com>
2025-06-02 11:14:04 -0300

    Update registro.hpp

--- include/registro.hpp
+++ include/registro.hpp
@@ -26,9 +26,31 @@ void setPontuacao (int novaPontuacao){
         pontuacao=novaPontuacao;
 }
 
-;}
+//Funções para o rank de jogadores
 
+void salvarRankingEmArquivo(const std::vector<Jogador>& ranking, const std::string& nomeArquivo);
+void carregarRankingDoArquivo(std::vector<Jogador>& ranking, const std::string& nomeArquivo);
+void ordenarRanking(std::vector<Jogador>& ranking);
+void exibirRanking(const std::vector<Jogador>& ranking);
+;};
+//Uso de "jogador".
+//Leitura e persistência dos dados do vetor "jogador".
+void salvarRankingEmArquivo(const std::vector<Jogador>& ranking, const std::string& nomeArquivo)
+{
+    std::ofstream arquivo(nomeArquivo);
 
+    if (!arquivo.is_open()) {
+        std::cerr << "Erro ao abrir o arquivo para escrita.\n";
+        return;
+    }
+
+    for (const auto& jogador : ranking) {
+        arquivo << jogador.getNome() << " " << jogador.getPontuacao() << "\n";
+    }
+
+    arquivo.close();
+}
 
 
 ;
+#endif
