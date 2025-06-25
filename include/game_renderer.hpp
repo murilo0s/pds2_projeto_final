5b51d2a9c332de5c5adff998ccdaf7517329a0c5
Guilherme Gaspar <gui.m.gaspar@gmail.com>
2025-06-24 22:12:24 -0300

    Adicionando documentação

--- include/game_renderer.hpp
+++ include/game_renderer.hpp
@@ -20,10 +20,10 @@ class Game;
  */
 class GameRenderer {
 private:
-    ALLEGRO_FONT* font; // Fonte para escrever textos
-    ALLEGRO_BITMAP* background_img; // Imagem de fundo do jogo
-    ALLEGRO_BITMAP* ground_img; // Imagem do chão
-    ALLEGRO_BITMAP* gameover_bg_img; // Imagem de fundo para Game Over
+    ALLEGRO_FONT* font; ///< Fonte para escrever textos
+    ALLEGRO_BITMAP* background_img; ///< Imagem de fundo do jogo
+    ALLEGRO_BITMAP* ground_img; ///< Imagem do chão
+    ALLEGRO_BITMAP* gameover_bg_img; ///< Imagem de fundo para Game Over
 
 public:
     /**
@@ -41,22 +41,93 @@ public:
      */
     ~GameRenderer();
 
-    // Desenha as telas do jogo
-    void renderMenu(const Game& game);      // Desenha o menu
-    void renderPlaying(const Game& game);   // Desenha o jogo
-    void renderGameOver(const Game& game);  // Desenha game over
-    void renderPaused(const Game& game);    // Desenha tela de pausa
+    /**
+     * @brief Desenha o menu do jogo
+     * 
+     * @param game Referencia o objeto game
+     */
+    void renderMenu(const Game& game);    
+    /**
+     * @brief Desenha o jogo propriamente dito
+     * 
+     * @param game Referencia o objeto game
+     */
+    void renderPlaying(const Game& game);   
+    /**
+     * @brief Desenha a tela de game over
+     * 
+     * @param game Referencia o objeto game
+     */
+    void renderGameOver(const Game& game); 
+    /**
+     * @brief Desenha a tela de pause do jogo
+     * 
+     * @param game Referencia o objeto game
+     */
+    void renderPaused(const Game& game);    
 
-    // Desenha elementos específicos
-    void renderScore(const Game& game);     // Desenha a pontuação
-    void renderBackground();                // Desenha o fundo
-    void renderUI(const Game& game);        // Desenha a interface
-    void centerText(const char* text, float y); // Centraliza texto
-    void renderCadastro(const Game& game); // Desenha a tela de cadastro
-    void renderAreaDeInput(const std::string& value, int x, int y, bool active); // Desenha a caixa de entrada de texto
+    /**
+     * @brief Desenha a pontuação do jogador
+     * 
+     * @param game Referencia o objeto game
+     */
+    void renderScore(const Game& game);
+    /**
+     * @brief Desenha a tela de fundo do jogo
+     * 
+     */
+    void renderBackground();                
+    /**
+     * @brief Desenha a interface gráfica do jogo
+     * 
+     * @param game Referencia o objeto game
+     */
+    void renderUI(const Game& game);        
+    /**
+     * @brief Centralização do texto
+     * 
+     * @param text Referencia o texto
+     * @param y 
+     */
+    void centerText(const char* text, float y); 
+    /**
+     * @brief Desenha a tela de cadastro de jogadores
+     * 
+     * @param game Referencia o objeto game
+     */
+    void renderCadastro(const Game& game); 
+    /**
+     * @brief Desenha a caixa de entrada de texto para cadastro de jogadores
+     * 
+     * @param value Referencia o valor de entrada
+     * @param x Referencia a posição x
+     * @param y Referencia a posição y
+     * @param active 
+     */
+    void renderAreaDeInput(const std::string& value, int x, int y, bool active); 
+    /**
+     * @brief Desenha a tela de seleção de jogadores
+     * 
+     * @param game Referencia o objeto game
+     */
     void renderSelecaoJogador(const Game& game); // Desenha a tela de seleção de jogador
-    void renderRanking(const Game& game); // Desenha a tela de ranking
+    /**
+     * @brief Desenha a tela de ranking dos jogadores com base nas pontuações prévias
+     * 
+     * @param game Referencia o objeto game
+     */
+    void renderRanking(const Game& game);
+    /**
+     * @brief Desenha o menu de jogadores
+     * 
+     * @param game Referencia o objeto game 
+     */
     void renderPlayerMenu(const Game& game);
+    /**
+     * @brief Desenha a tela de cadastro dos jogadores
+     * 
+     * @param game Referencia o objeto game
+     */
     void renderCadastroJogador(const Game& game);
 };
 
